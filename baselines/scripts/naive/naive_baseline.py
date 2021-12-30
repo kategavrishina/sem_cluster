7cad0221a0bf85fe08a85597d8175ae49c6ccc52
Elisejj <elisricov@yandex.ru>
2021-12-30 17:16:06 +0300

    naive baselines

--- baselines/scripts/naive/naive_baseline.py
+++ baselines/scripts/naive/naive_baseline.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import numpy as np
-from sklearn.metrics import adjusted_rand_score
+from sklearn.metrics import accuracy_score
 
 
 def first_sense(dataset: pd.DataFrame):
     dummy_pred = [1] * len(dataset)
-    print(f"ARI first sense for each: {adjusted_rand_score(dummy_pred, dataset['gold_sense_id'])}")
+    print(f"Accuracy first sense for each: {accuracy_score(dummy_pred, dataset['gold_sense_id'])}")
 
 
 def random_sense(dataset: pd.DataFrame):
     random_pred = [np.random.randint(1, 3) for _ in range(len(dataset))]
-    print(f"ARI random sense: {adjusted_rand_score(random_pred, dataset['gold_sense_id'])}")
+    print(f"Accuracy random sense: {accuracy_score(random_pred, dataset['gold_sense_id'])}")
 
 
 def run_all_naive_baselines(path_to_dataset: str):
